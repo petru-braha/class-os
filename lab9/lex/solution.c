449530883f6ceb837017ee9dcf0214eb64b4d515
P <petrubraha@gmail.com>
2025-04-30 19:39:48 +0300

    started working on lab9;

---
+++ lab9/lex/solution.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+  if (3 != argc)
+  {
+    fprintf(stderr, "error: wrong number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  char *file = argv[1];
+  int N = atoi(argv[2]);
+
+  if (-1 == access(file, W_OK))
+  {
+    fprintf(stderr, "error: invalid file");
+    exit(EXIT_FAILURE);
+  }
+
+  if (N <= 0)
+  {
+    fprintf(stderr, "error: invalid n");
+    exit(EXIT_FAILURE);
+  }
+
+  // file N
+  pid_t pid = 0;
+  if (-1 == (pid = fork()))
+  {
+    fprintf(stderr, "error: fork() failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (0 == pid)
+  {
+    // asteapta SIGUSR2
+    // printf("primt");
+    // printf("gata");
+  }
+  else
+  {
+    int fd = open(file, O_WRONLY);
+    if (-1 == fd)
+    {
+      fprintf(stderr, "error - parent: open() failed");
+      exit(EXIT_FAILURE);
+    }
+
+    // alarm 5 sec - no input
+    // if(no input in 5 * N secunde)
+    //   close
+    //   kill(pid, SIGUSR2);
+    //   printf(bad stuff);
+    //   wait for SIGCHLD
+    //   exit(1);
+    // if(ctrl d)
+    //   close
+    //   kill(pid, SIGUSR2);
+    //   wait for SIGCHLD
+    //   exit(0)
+    // Parinte: sfarsit executie!"
+  }
+
+  return EXIT_SUCCESS;
+}
