fdde783737df04c0606a5636252430bebe6c8d0d
pierre <pierre@pierre.pierre.com>
2025-06-09 20:38:42 +0300

    trying my best

---
+++ test/starter.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# input validation
+if [ $# -ne 1 ]; then
+  echo "error: wrong number of parameters"
+  exit 1
+fi
+
+# compilation
+gcc ./subordinates/worker1.c -o ./subordinates/worker1.out
+if [ $? -ne 0 ]; then
+  echo "error: compilation failed"
+  exit 3
+fi
+
+gcc ./subordinates/worker2.c -o ./subordinates/worker2.out
+if [ $? -ne 0 ]; then
+  echo "error: compilation failed"
+  exit 3
+fi
+
+# program launch
+p=$1
+$file="./input_data.txt"
+if [ $p -eq 1 ]; then
+  ./subordinates/worker1.out $file &
+  sleep 2
+  ./subordinates/worker2.out
+
+elif [ $p -eq 2 ]; then
+  ./subordinates/worker2.out &
+  sleep 3
+  ./subordinates/worker1.out $file
+
+else
+  echo "error: wrong paramter"
+  exit 2
+fi
+
+# todo clean the workspace
+echo "all three processes ended"
+# echo conținutul mapării nepersistente cu nume
+# delete maparea nepersistentă cu nume
+# delete canalul fifo
